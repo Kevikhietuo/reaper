8d429d64e740e2a7ed39853b294f2c574bad0c72
unknown <kevikhietuokesiezie@gmail.com>
2022-08-13 18:08:07 +0530

    UI updates

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fileinput import filename
-from importlib.resources import path
-from tkinter import *
 import tkinter.font as font
+
+from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+from fileinput import filename
+from importlib.resources import path
+
 
 def selectFile():
     global pathName
@@ -21,14 +23,15 @@ def showButton():
 if __name__ == "__main__":
     win = Tk()
     win.geometry("990x450") #window size
+    win["bg"] = "cornsilk"
     myPath = StringVar() #soon-to-be global var
     
-    Label(win, text="Select the folder inside which your reference papers are stored", font=('Times 15')).pack(pady=50)
+    Label(win, text="Select the folder inside which your reference papers are stored", font=('Times 15'), bg='cornsilk').pack(pady=50)
     custFont = font.Font(family='Times')
     
     button = Button(text="BROWSE",height=2,width=40, fg='white', bg='light slate grey', command=selectFile)
     button['font'] = custFont
     button.pack()
-    
+
     win.mainloop()
     print(pathName)
