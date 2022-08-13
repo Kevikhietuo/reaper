644a94857922465e2e7c6ebc13b570c12fae43ca
unknown <kevikhietuokesiezie@gmail.com>
2022-08-13 17:34:00 +0530

    global value issue solved

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+from fileinput import filename
+from importlib.resources import path
 from tkinter import *
+import tkinter.font as font
 from tkinter import ttk
 from tkinter import filedialog
+
+def selectFile():
+    global filename
+    filename = filedialog.askdirectory()
+    Label(win, text=filename, font=('Times 15')).pack()
 
 if __name__ == "__main__":
     win = Tk()
     win.geometry("990x450") #window size
-    def selectFile():
-        path= filedialog.askdirectory()
-        Label(win, text=path, font=13).pack()
+    myPath = StringVar() #soon-to-be global var
     
-    Label(win, text="SELECT THE FOLDER INSIDE WHICH YOUR REFERENCE PAPERS ARE STORED", font=('Times 12 bold')).pack(pady=20)
-    
-    button= ttk.Button(win, text="BROWSE", command= selectFile)
-    button.pack(ipadx=5, pady=15)
-    
+    Label(win, text="Select the folder inside which your reference papers are stored", font=('Times 15')).pack(pady=50)
+    custFont = font.Font(family='Times')
+    button = Button(text="BROWSE",height=2,width=40, fg='white', bg='light slate grey', command=selectFile)
+    button['font'] = custFont
+    button.pack()
     win.mainloop()
+    print(filename)
