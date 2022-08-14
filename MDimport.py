578f8fa1e11aef009cf8e191c7058c480bbe1db8
unknown <kevikhietuokesiezie@gmail.com>
2022-08-14 19:47:05 +0530

    path changed

--- MDimport.py
+++ MDimport.py
@@ -6,10 +6,8 @@ def metadataImport(pathName):
     pdfFiles = glob.glob(pathName + '/*.pdf')
     
     for pdf in pdfFiles:
-        print("CURRENT FILE: ", str(pdf))
         with open(pdf, 'rb') as f:
             pdf = PdfFileReader(f)
             info = pdf.getDocumentInfo()
 
-            insertData.insert_data(info)
-        print("CURRENT FILE DONE \n")
+            insertData.insert_data(info, pathName)
--- insertData.py
+++ insertData.py
@@ -3,27 +3,21 @@ from csv import writer
 import os
 import pandas as pd
 
-def insert_data(info):
+def insert_data(info, pathName):
     filename = 'reaper.csv'
-    if not (os.path.isfile(filename)): #file does not exists
+    myPath = str(pathName) + "/" + filename
+    print("THE PATH IS: ", myPath)
+    
+    if not (os.path.isfile(myPath)): #file does not exists
         headers = ['author', 'creator', 'producer', 'subject', 'title']
 
-        with open(filename, 'w', newline='') as csvfile:
+        with open(myPath, 'w', newline='') as csvfile:
             csvwriter = csv.writer(csvfile)
             csvwriter.writerow(headers)
             csvfile.close()
     
     row = [info.author, info.creator, info.producer, info.subject, info.title]
     
-    with open(filename, 'a+', newline='') as write_obj:
+    with open(myPath, 'a+', newline='') as write_obj:
         csv_writer = writer(write_obj)
         csv_writer.writerow(row)
-
-    """
-    df = pd.DataFrame({ 'author': [info.author],
-                        'creator':[info.creator],
-                        'producer':[info.producer],
-                        'subject':[info.subject],
-                        'title':[info.title]})
-    df.to_csv(filename, index=False)
-    """
