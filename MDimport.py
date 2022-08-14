217a99f3e345891364cc3382bf4de6b391a062f0
unknown <kevikhietuokesiezie@gmail.com>
2022-08-14 19:38:50 +0530

    reverted back to CSVs

--- MDimport.py
+++ MDimport.py
@@ -6,8 +6,10 @@ def metadataImport(pathName):
     pdfFiles = glob.glob(pathName + '/*.pdf')
     
     for pdf in pdfFiles:
+        print("CURRENT FILE: ", str(pdf))
         with open(pdf, 'rb') as f:
             pdf = PdfFileReader(f)
             info = pdf.getDocumentInfo()
 
             insertData.insert_data(info)
+        print("CURRENT FILE DONE \n\n")
--- insertData.py
+++ insertData.py
@@ -1,29 +1,21 @@
-import xlsxwriter
+import csv
+from csv import DictWriter
 import os
 import pandas as pd
 
 def insert_data(info):
-    filename = 'reaper.xlsx'
-    if (os.path.isfile(filename)): #file already exists
-        workbook = xlsxwriter.Workbook(filename)
-        worksheet = workbook.add_worksheet()
-        worksheet.write('author', info.author)
-        worksheet.write('creator', info.creator)
-        worksheet.write('producer', info.producer)
-        worksheet.write('subject', info.subject)
-        worksheet.write('title', info.title)
+    filename = 'reaper.csv'
+    if not (os.path.isfile(filename)): #file does not exists
+        headers = ['author', 'creator', 'producer', 'subject', 'title']
 
-        workbook.close()
+        with open(filename, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(headers)
+            csvfile.close()
 
-        # df = pd.DataFrame({ 'author': [info.author],
-        #                     'creator':[info.creator],
-        #                     'producer':[info.producer],
-        #                     'subject':[info.subject],
-        #                     'title':[info.title]})
-        # df.to_excel(filename, sheet_name='reapers', index=False)
-        
-    else: #file does not exist
-        writer = pd.ExcelWriter(filename, engine='xlsxwriter')
-        writer.save()
-        df = pd.read_excel(filename)
-        df.columns = ['author', 'creator', 'producer', 'subject', 'title']
+    df = pd.DataFrame({ 'author': [info.author],
+                        'creator':[info.creator],
+                        'producer':[info.producer],
+                        'subject':[info.subject],
+                        'title':[info.title]})
+    df.to_csv(filename, index=False)
