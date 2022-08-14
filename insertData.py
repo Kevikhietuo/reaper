37ef8cc58202991bc83526219e01ff64b6dbfaf4
unknown <kevikhietuokesiezie@gmail.com>
2022-08-14 18:29:24 +0530

    update

--- insertData.py
+++ insertData.py
@@ -4,11 +4,11 @@ import pandas as pd
 def insert_data(info):
     filename = 'reaper.xlsx'
     if (os.path.isfile(filename)): #file already exists
-        df = pd.DataFrame({ 'author':info.author,
-                            'creator':info.creator,
-                            'producer':info.producer,
-                            'subject': info.subject,
-                            'title':info.title})
+        df = pd.DataFrame({ 'author':[info.author],
+                            'creator':[info.creator],
+                            'producer':[info.producer],
+                            'subject':[info.subject],
+                            'title':[info.title]})
         df.to_excel(filename, sheet_name='reapers', index=False)
         
     else: #file does not exist
